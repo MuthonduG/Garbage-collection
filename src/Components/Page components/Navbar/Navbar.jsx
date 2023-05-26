b45dd7c110a86385c7c5a5e361ca95753cd3d986
MuthonduG <muthondugithinji@gmail.com>
2023-05-26 12:35:04 +0300

    navbar initial commit

--- src/Components/Page components/Navbar/Navbar.jsx
+++ src/Components/Page components/Navbar/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './navbar.css'
+import Icon from '../../../Assets /icon2.png'
+import {navlinks} from '../../../data'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <section className="navbar__container-holder">
+        <div className="navbar__container">
+          <div className="navbar__icon">
+            <img src={Icon} alt="" />
+            <h3>Taka</h3>
+          </div>
+          <div className="nav__links">
+          {
+            navlinks && navlinks.map(({name, path}, index)=> {
+                return (
+                    <li key={index}>
+                        <a href = {path}>{name}</a>
+                    </li>
+                )
+              })
+            }
+          </div>
+          <div className="nav__buttons">
+            <a href="!#" className="btn sm">Log in</a>
+            <a href="!#" className="btn sm">Sign up</a>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const navlinks = [
+    {
+        path: '#',
+        name: 'Home'
+    }, 
+    {
+        path: '#',
+        name: 'About'
+    },
+    {
+        path: '#',
+        name: 'Team'
+    },
+    {
+        path: '#',
+        name: 'Contact us'
+    }
+]
